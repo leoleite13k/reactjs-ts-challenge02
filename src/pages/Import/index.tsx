631cf28d601345ce5c7e88f4aacb435a0f44702c
Leonardo Leite <leonardotleite13k@gmail.com>
2020-05-27 08:32:54 -0300

    feature: Finished challenge 02 with ReactJS and TS

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.map(uploadFile => {
+      data.append('file', uploadFile.file);
+
+      return null;
+    });
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const newFile = {
+      file: files[0],
+      name: files[0].name,
+      readableSize: filesize(files[0].size),
+    };
+
+    setUploadedFiles([...uploadedFiles, newFile]);
   }
 
   return (
